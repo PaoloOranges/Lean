f2841ebff39ddbd9cd95dafb4931be6a750b82ea
Paolo Oranges <paolo.oranges@gmail.com>
2020-04-19 13:44:42 +0200

    fix is warming up
    update btcusd_trade daily

--- Algorithm.CSharp/PaoloTestCryptoAlgorithm.cs
+++ Algorithm.CSharp/PaoloTestCryptoAlgorithm.cs
@@ -30,10 +30,12 @@ namespace QuantConnect.Algorithm.CSharp
     /// <meta name="tag" content="using data" />
     /// <meta name="tag" content="using quantconnect" />
     /// <meta name="tag" content="trading and orders" />
-    public class PaoloTestCryptoAlgorithm : QCAlgorithm, IRegressionAlgorithmDefinition
+    public class PaoloTestCryptoAlgorithm : QCAlgorithm
     {
         private ExponentialMovingAverage _fast;
         private ExponentialMovingAverage _slow;
+        private MovingAverageConvergenceDivergence _macd;
+        private AverageDirectionalIndex _adx;
 
         private bool _bought = false;
         readonly string SymbolName = "BTCUSD";
@@ -44,7 +46,8 @@ namespace QuantConnect.Algorithm.CSharp
         /// </summary>
         public override void Initialize()
         {
-            SetStartDate(2020, 1, 1); // Set Start Date
+            Resolution resolution = Resolution.Daily;
+            SetStartDate(2019, 2, 5); // Set Start Date
             SetEndDate(2020, 4, 1); // Set End Date
                        
             // Set Strategy Cash (EUR)
@@ -59,20 +62,25 @@ namespace QuantConnect.Algorithm.CSharp
 
             SetBrokerageModel(BrokerageName.GDAX, AccountType.Cash);
 
-            //SetWarmUp(new TimeSpan(24, 0, 0));
+            
             // You can uncomment the following line when live trading with GDAX,
             // to ensure limit orders will only be posted to the order book and never executed as a taker (incurring fees).
             // Please note this statement has no effect in backtesting or paper trading.
             // DefaultOrderProperties = new GDAXOrderProperties { PostOnly = true };
 
             // Find more symbols here: http://quantconnect.com/data
-            _symbol = AddCrypto(SymbolName, Resolution.Hour, Market.GDAX).Symbol;
+            _symbol = AddCrypto(SymbolName, resolution, Market.GDAX).Symbol;
             
 
             // create two moving averages
-            _fast = EMA(_symbol, 5, Resolution.Hour);
-            _slow = EMA(_symbol, 14, Resolution.Hour);
+            _fast = EMA(_symbol, 5, resolution);
+            _slow = EMA(_symbol, 15, resolution);
+            _macd = MACD(_symbol, 5, 25, 15, MovingAverageType.Exponential, resolution);
+            _adx = ADX(_symbol, 15, resolution);
+
             _bought = false;
+
+            SetWarmUp(30);
         }
 
         /// <summary>
@@ -90,34 +98,49 @@ namespace QuantConnect.Algorithm.CSharp
                 throw new Exception("Conversion rate is 0");
             }
 
-    
-            
-                // To include any initial holdings, we read the LTC amount from the cashbook
-                // instead of using Portfolio["LTCUSD"].Quantity
+            if(IsWarmingUp)
+            {
+                return;
+            }
 
-                if (_fast > _slow)
+            if(!_bought)
+            {
+                if(_adx > 15.0)
                 {
-                    if(!_bought)
-                    {
-                        decimal btcPrice = Securities[SymbolName].Price;
-                        decimal quantity = Math.Round(Portfolio.CashBook["USD"].Amount / btcPrice, 2);
-                        Buy(_symbol, quantity);
-                        _bought = true;
-                    }
+                    var a = _adx;
                 }
-                else
-                {
-                    if (Portfolio.CashBook["BTC"].Amount > 0)
-                    {
-                        // The following two statements currently behave differently if we have initial holdings:
-                        // https://github.com/QuantConnect/Lean/issues/1860
+            }
+            else
+            {
 
-                        Liquidate(_symbol);
-                        // SetHoldings("LTCUSD", 0);
-                        _bought = false;
-                    }
-                }
-            
+            }
+
+            // To include any initial holdings, we read the LTC amount from the cashbook
+            // instead of using Portfolio["LTCUSD"].Quantity
+
+            //if (_fast > _slow)
+            //{
+            //    if (!_bought)
+            //    {
+            //        decimal btcPrice = Securities[SymbolName].Price;
+            //        decimal quantity = Math.Round(Portfolio.CashBook["USD"].Amount / btcPrice, 2);
+            //        Buy(_symbol, quantity);
+            //        _bought = true;
+            //    }
+            //}
+            //else
+            //{
+            //    if (Portfolio.CashBook["BTC"].Amount > 0)
+            //    {
+            //        // The following two statements currently behave differently if we have initial holdings:
+            //        // https://github.com/QuantConnect/Lean/issues/1860
+
+            //        Liquidate(_symbol);
+            //        // SetHoldings("LTCUSD", 0);
+            //        _bought = false;
+            //    }
+            //}
+
         }
 
         public override void OnOrderEvent(OrderEvent orderEvent)
@@ -130,62 +153,5 @@ namespace QuantConnect.Algorithm.CSharp
             Log($"{Time} - TotalPortfolioValue: {Portfolio.TotalPortfolioValue}");
             Log($"{Time} - CashBook: {Portfolio.CashBook}");
         }
-
-        /// <summary>
-        /// This is used by the regression test system to indicate if the open source Lean repository has the required data to run this algorithm.
-        /// </summary>
-        public bool CanRunLocally { get; } = true;
-
-        /// <summary>
-        /// This is used by the regression test system to indicate which languages this algorithm is written in.
-        /// </summary>
-        public Language[] Languages { get; } = { Language.CSharp, Language.Python };
-
-        /// <summary>
-        /// This is used by the regression test system to indicate what the expected statistics are from running the algorithm
-        /// </summary>
-        public Dictionary<string, string> ExpectedStatistics => new Dictionary<string, string>
-        {
-            {"Total Trades", "10"},
-            {"Average Win", "0%"},
-            {"Average Loss", "0%"},
-            {"Compounding Annual Return", "0%"},
-            {"Drawdown", "0%"},
-            {"Expectancy", "0"},
-            {"Net Profit", "0%"},
-            {"Sharpe Ratio", "0"},
-            {"Probabilistic Sharpe Ratio", "0%"},
-            {"Loss Rate", "0%"},
-            {"Win Rate", "0%"},
-            {"Profit-Loss Ratio", "0"},
-            {"Alpha", "0"},
-            {"Beta", "0"},
-            {"Annual Standard Deviation", "0"},
-            {"Annual Variance", "0"},
-            {"Information Ratio", "0"},
-            {"Tracking Error", "0"},
-            {"Treynor Ratio", "0"},
-            {"Total Fees", "$85.33"},
-            {"Fitness Score", "0.5"},
-            {"Kelly Criterion Estimate", "0"},
-            {"Kelly Criterion Probability Value", "0"},
-            {"Sortino Ratio", "79228162514264337593543950335"},
-            {"Return Over Maximum Drawdown", "-43.917"},
-            {"Portfolio Turnover", "1.028"},
-            {"Total Insights Generated", "0"},
-            {"Total Insights Closed", "0"},
-            {"Total Insights Analysis Completed", "0"},
-            {"Long Insight Count", "0"},
-            {"Short Insight Count", "0"},
-            {"Long/Short Ratio", "100%"},
-            {"Estimated Monthly Alpha Value", "$0"},
-            {"Total Accumulated Estimated Alpha Value", "$0"},
-            {"Mean Population Estimated Insight Value", "$0"},
-            {"Mean Population Direction", "0%"},
-            {"Mean Population Magnitude", "0%"},
-            {"Rolling Averaged Population Direction", "0%"},
-            {"Rolling Averaged Population Magnitude", "0%"},
-            {"OrderListHash", "1073240275"}
-        };
     }
 }
